9030203c0359e4e3315cf3c9eaad705d5ee19bdb
NotStud <119549347+NotStud@users.noreply.github.com>
2022-11-30 20:32:08 -0500

    Update First code using VS.py

--- First code using VS.py
+++ First code using VS.py
@@ -1,5 +1,7 @@
 print('Hello World')
 
 guess = int(input('Enter a number of trails you want to make: '))
-for x in range(guess):
-    print('Hello Dylan')
+while guess > 0:
+    for x in range(guess):
+        print('Hello Dylan')
+     
